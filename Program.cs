29f7b8fed48143d29b152ee12b0376f44b148bfe
Oscar Davies <oscar.william.davies@outlook.com>
2021-07-04 17:27:44 +0100

    Improve Code Quality

--- Program.cs
+++ Program.cs
@@ -1,68 +1,48 @@
-﻿using System;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using System;
 
 namespace GridGenerator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Methods
+
+        private static void ReadAndProcessValue(out int val)
         {
-            Console.WriteLine("Square Height: ");
-            string str_sh = Console.ReadLine();
-            int sh;
-            if (!int.TryParse(str_sh, out sh))
+            string s = Console.ReadLine();
+            if (!int.TryParse(s,out val))
             {
-                Console.WriteLine("Inavlid Value");
-                Main(args);
+                Console.WriteLine("Invalid Value!");
+                Main(null);
             }
+        }
 
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Square Height (px): ");
+            ReadAndProcessValue(out int sh);
 
-            Console.WriteLine("Square Width: ");
-            string str_sw = Console.ReadLine();
-            int sw;
-            if (!int.TryParse(str_sw, out sw))
-            {
-                Console.WriteLine("Inavlid Value");
-                Main(args);
-            }
-
-
+            Console.WriteLine("Square Width (px): ");
+            ReadAndProcessValue(out int sw);
 
             Console.WriteLine("Squares High: ");
-            string str_shc = Console.ReadLine();
-            int shc;
-            if (!int.TryParse(str_shc, out shc))
-            {
-                Console.WriteLine("Inavlid Value");
-                Main(args);
-            }
-
+            ReadAndProcessValue(out int shc);
 
             Console.WriteLine("Squares Wide: ");
-            string str_swc = Console.ReadLine();
-            int swc;
-            if (!int.TryParse(str_swc, out swc))
-            {
-                Console.WriteLine("Inavlid Value");
-                Main(args);
-            }
+            ReadAndProcessValue(out int swc);
 
-            Console.WriteLine("Edge Thickness: ");
-            string str_sthick = Console.ReadLine();
-            int sthick;
-            if (!int.TryParse(str_sthick, out sthick))
-            {
-                Console.WriteLine("Inavlid Value");
-                Main(args);
-            }
+            Console.WriteLine("Edge Thickness (px): ");
+            ReadAndProcessValue(out int sthick);
 
-            Image<Rgba32> image = new Image<Rgba32>((swc * sw) + (sthick/2), (shc * sh) + (sthick/2));
+            Console.WriteLine("Creating Image, please wait....");
+
+            Image<Rgba32> image = new Image<Rgba32>((swc * sw) + (sthick / 2), (shc * sh) + (sthick / 2));
             for (int x = 0, y = 0; y < sh;)
             {
-                Rectangle r = new Rectangle(x*sw,y*sh,sw,sh);
+                Rectangle r = new Rectangle(x * sw, y * sh, sw, sh);
                 image.Mutate(x => x.Draw(Color.Black, sthick, r));
 
                 x++;
@@ -73,11 +53,14 @@ namespace GridGenerator
                 }
             }
 
-            Console.WriteLine("File name: ");
+            Console.WriteLine("Desired File Name: ");
             string fname = Console.ReadLine();
             image.SaveAsPng($"./{fname}.png");
 
-            Console.WriteLine("Saved Grid Image");
+            Console.WriteLine("Saved Your Grid Image!");
+            Console.ReadLine();
         }
+
+        #endregion Methods
     }
 }
